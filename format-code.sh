4197c5f8ed0d2bc24893b17a2cb1e4e1bec3ebd0
Tyler Veness <calcmogul@gmail.com>
2015-08-22 17:11:09 -0700

    vim swap files are now ignored and uncrustify is no longer run on qcustomplot

--- format-code.sh
+++ format-code.sh
@@ -1,29 +1,8 @@
 #!/bin/bash
-NAME=${0##*/}
 
-args() {
-  echo >&2 "$@"
-  echo usage: $NAME COMMAND
-  echo -e "COMMAND can be either 'format' or 'clean'"
+# format C++ code
+find $PWD ! -type d -not \( -name .svn -prune -o -name .hg -prune -o -name .git -prune -o -name ".*" -prune -o -name Makefile -prune -o -name moc_\* -prune -o -name ui_\* -prune -o -name qrc_\* -prune -o -name qcustomplot.\* -prune \) -a \( -name \*.cpp -o -name \*.hpp -o -name \*.inl \) | uncrustify -c uncrustify.cfg -F - -l CPP --replace --no-backup
 
-  echo $NAME format
-  echo -e "\tformat C/C++ code\n"
-}
-
-format() {
-  find $PWD ! -type d -not \( -name .svn -prune -o -name .hg -prune -o -name .git -prune -o -name ".*" -prune -o -name Makefile -prune -o -name moc_\* -prune -o -name ui_\* -prune -o -name qrc_\* -prune \) -a \( -name \*.cpp -o -name \*.hpp -o -name \*.inl \) | uncrustify -c uncrustify.cfg -F - -l CPP --replace --no-backup
-  find $PWD ! -type d -not \( -name .svn -prune -o -name .hg -prune -o -name .git -prune -o -name ".*" -prune -o -name Makefile -prune -o -name moc_\* -prune -o -name ui_\* -prune -o -name qrc_\* -prune \) -a \( -name \*.c -o -name \*.h \) | uncrustify -c uncrustify.cfg -F - -l C --replace --no-backup
-}
-
-if [ $# = 0 ] ; then
-  format
-elif [ $# = 1 ] ; then
-  if [ "$1" = "format" ] ; then
-    format
-  else
-    args $NAME: invalid command \'$1\'
-  fi
-else
-  args $NAME: wrong number of args \(expected 1\; got $#\)
-fi
+# format C code
+find $PWD ! -type d -not \( -name .svn -prune -o -name .hg -prune -o -name .git -prune -o -name ".*" -prune -o -name Makefile -prune -o -name moc_\* -prune -o -name ui_\* -prune -o -name qrc_\* -prune -o -name qcustomplot.\* -prune \) -a \( -name \*.c -o -name \*.h \) | uncrustify -c uncrustify.cfg -F - -l C --replace --no-backup
 
