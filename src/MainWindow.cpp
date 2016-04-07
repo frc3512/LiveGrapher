79d732ae5cc2672c4c08905628d83a0ce1d91163
Tyler Veness <calcmogul@gmail.com>
2016-04-06 21:40:18 -0700

    The graph axes are now rescaled every 250ms instead of whenever new data points are received.
    Change-Id: Ic190d167131730fb77655945649e929f1cf403f0

--- src/MainWindow.cpp
+++ src/MainWindow.cpp
@@ -1,5 +1,7 @@
 #include "MainWindow.hpp"
 
+using namespace std::chrono_literals;
+
 MainWindow::MainWindow(QWidget* parent) :
     QMainWindow(parent),
     m_graph(this),
@@ -43,6 +45,7 @@ MainWindow::MainWindow(QWidget* parent) :
             this, SLOT(realtimeDataSlot(int, float, float)));
 
     m_xHistory = m_settings.getDouble("xHistory");
+    m_lastTime = std::chrono::steady_clock::now();
 }
 
 void MainWindow::infoDialog(const QString& title, const QString& text) {
@@ -80,17 +83,21 @@ void MainWindow::realtimeDataSlot(int graphId, float x, float y) {
     // Add data to lines
     plot->graph(graphId)->addData(x, y);
 
-    for (int i = 0; i < plot->graphCount(); i++) {
-        // Rescale value (vertical) axis to fit the current data
-        if (i == 0) {
-            plot->graph(i)->rescaleValueAxis();
-        }
-        else {
-            plot->graph(i)->rescaleValueAxis(true);
+    if (std::chrono::steady_clock::now() - m_lastTime > 250ms) {
+        for (int i = 0; i < plot->graphCount(); i++) {
+            // Rescale value (vertical) axis to fit the current data
+            if (i == 0) {
+                plot->graph(i)->rescaleValueAxis();
+            }
+            else {
+                plot->graph(i)->rescaleValueAxis(true);
+            }
+
+            // Remove data of lines that are outside visible range
+            plot->graph(i)->removeDataBefore(x - m_xHistory);
         }
 
-        // Remove data of lines that are outside visible range
-        plot->graph(i)->removeDataBefore(x - m_xHistory);
+        m_lastTime = std::chrono::steady_clock::now();
     }
 
     // Make key axis range scroll with the data (at a constant range size)
