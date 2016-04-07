6911e9f1e6045c592af7c3f711bc580a219d388f
Tyler Veness <calcmogul@gmail.com>
2016-04-06 21:24:09 -0700

    Added LiveGrapher host files to repository
    Change-Id: I9e72994656f1098bcd7e3cfaeccdb3ae8fd1b111

---
+++ host/LiveGrapher/SocketConnection.hpp
@@ -0,0 +1,58 @@
+// =============================================================================
+// Description: Wrapper around graph client socket descriptors
+// Author: FRC Team 3512, Spartatroniks
+// =============================================================================
+
+#ifndef SOCKET_CONNECTION_HPP
+#define SOCKET_CONNECTION_HPP
+
+#include <cstdint>
+#include <queue>
+#include <string>
+#include <vector>
+
+class SocketConnection {
+public:
+    enum selector {
+        Read = 1,
+        Write = 2,
+        Error = 4
+    };
+
+    SocketConnection(int nfd, int ipcWriteSock);
+    ~SocketConnection();
+    SocketConnection(const SocketConnection&) = delete;
+    SocketConnection& operator=(const SocketConnection&) = delete;
+
+    int readPackets();
+    void processPacket(std::string& buf);
+    void sendList();
+    void writePackets();
+    template <class T>
+    void queueWrite(T& buf);
+
+    // Contains all graphs
+    static std::vector<std::string> graphNames;
+
+    int fd;
+    uint8_t selectflags = Read | Error;
+    std::vector<std::string> datasets;
+
+private:
+    int m_ipcfd_w;
+
+    // Write buffer currently being written
+    std::string m_writebuf; // The buffer that needs to be written into the socket
+    size_t m_writebufoffset = 0; // How much has been written so far
+    bool m_writedone = true;
+    std::queue<std::string> m_writequeue;
+
+    // Read buffer currently being read
+    std::string m_readbuf;
+    size_t m_readbufoffset = 0;
+    bool m_readdone = true;
+};
+
+#include "SocketConnection.inl"
+
+#endif // SOCKET_CONNECTION_HPP
---
+++ host/LiveGrapher/SocketConnection.inl
@@ -0,0 +1,15 @@
+// =============================================================================
+// Description: Wrapper around graph client socket descriptors
+// Author: FRC Team 3512, Spartatroniks
+// =============================================================================
+
+#include <unistd.h>
+
+template <class T>
+void SocketConnection::queueWrite(T& buf) {
+    m_writequeue.emplace(reinterpret_cast<const char*>(&buf), sizeof(T));
+
+    // Select on write
+    selectflags |= SocketConnection::Write;
+    write(m_ipcfd_w, "r", 1);
+}
