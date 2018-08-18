af065e41dddf91741265ef249e15f77a5db56c20
Tyler Veness <calcmogul@gmail.com>
2018-08-17 22:40:04 -0700

    Strip debug symbols from LiveGrapher.exe before zipping win32 build
    Change-Id: I724c1dc32ef0b317a1036848695243e99a435692

--- publish-win32.sh
+++ publish-win32.sh
@@ -18,6 +18,7 @@ mkdir -p $PROJECT-$DATE/platforms
 # Copy files into folder
 cp build/release/$PROJECT.exe /usr/$PREFIX/bin/{Qt5Core,Qt5Gui,Qt5PrintSupport,Qt5Network,Qt5Widgets,libbz2-1,libfreetype-6,libgcc_s_seh-1,libglib-2.0-0,libgraphite2,libharfbuzz-0,libiconv-2,libintl-8,libpcre-1,libpcre2-16-0,libpng16-16,libstdc++-6,libwinpthread-1,zlib1}.dll IPSettings.txt $PROJECT-$DATE
 cp /usr/$PREFIX/lib/qt/plugins/platforms/qwindows.dll $PROJECT-$DATE/platforms
+$PREFIX-strip $PROJECT-$DATE/$PROJECT.exe
 
 # Create archive
 zip -r $PROJECT-$DATE.zip $PROJECT-$DATE
